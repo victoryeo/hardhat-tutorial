dfccf8f27c73d2e65fb3fe8623e995dee652ed7b
yeosiowvic <victoryeo1974@gmail.com>
2021-08-23 09:49:11 +0800

    second commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# deploy contract
+npx hardhat run scripts/deploy.js --network development
+# upgrade contract
+npx hardhat run scripts/upgrade.js --network development
+# go to console to test the contract
+npx hardhat console --network development
+
+'''
+Welcome to Node.js v14.17.5.
+Type ".help" for more information.
+> const BoxV2 = await ethers.getContractFactory('BoxV2');
+undefined
+> const box = await BoxV2.attach('0x254dffcd3277C0b1660F6d42EFbB754edaBAbC2B')
+undefined
+> (await box.retrieve()).toString();
+'42'
+'''
---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+const { ethers, upgrades } = require('hardhat');
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Token = await ethers.getContractFactory("Token");
+  console.log('Deploying Token...');
+  const token = await Token.deploy();
+  console.log("Token address:", token.address);
+
+  const Box = await ethers.getContractFactory('Box');
+  console.log('Deploying Box...');
+  const box = await upgrades.deployProxy(Box, [42], { initializer: 'store' });
+  await box.deployed();
+  console.log('Box deployed to:', box.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
